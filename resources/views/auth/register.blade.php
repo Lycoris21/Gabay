85227626955aa2ca112d5aa398947b04549c3935
Kwesten <christineanndejito21@gmail.com>
2024-11-02 23:06:07 +0800

    updated styles for register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,20 @@
 <x-guest-layout>
+    <div>
+        <x-auth-header heading="Create an account"/>
+    </div>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        {{-- <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        </div> --}}
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Email address')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
@@ -39,14 +42,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex flex-col items-center justify-end mt-6">
 
-            <x-primary-button class="ms-4">
+            <x-primary-button class="mt-2 w-full">
                 {{ __('Register') }}
             </x-primary-button>
+
+            <a class="mt-10 flex flex-row text-sm font-semibold text-blue-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                <p class="font-light text-gray-500 mr-1">Already have an account?</p>
+                {{ __('Sign in') }}
+            </a>
         </div>
     </form>
 </x-guest-layout>
