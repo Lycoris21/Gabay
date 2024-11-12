e2f75e14a1b3215ae801ba8a5563d8f71265197c
Kwesten <christineanndejito21@gmail.com>
2024-11-13 01:11:43 +0800

    updated welcome.blade.php

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                customOrange: '#BD7D45', // or 'rgb(189, 125, 69)'
+            },
         },
     },
 
