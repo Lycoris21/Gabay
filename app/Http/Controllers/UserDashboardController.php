0ff836c5cb562d1f92bb47dd0dc5a3cbb6da1561
Whos-Andrei-Go <23104518@usc.edu.ph>
2024-12-15 04:11:45 +0800

    okay notifs work now

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Booking;
+use App\Models\User;
+use App\Models\BookingNotification;
 use Illuminate\Support\Facades\Auth;
 
 class UserDashboardController extends Controller
 {
-
     public function index()
     {
         $section = 'profile'; 
         $content = 'dashboard.userProfile'; 
+
+        if (Auth::check()) {
+            $notifications = BookingNotification::where('user_id', Auth::user() -> id)->get();
+        } else {
+            $notifications = collect();
+        }
 
         $upcomingSessions = Booking::where('tutee_id', Auth::id())
             ->whereDate('date', '>=', now())
@@ -21,7 +28,8 @@ class UserDashboardController extends Controller
 
         $subjectTags = ['Mathematics', 'English', 'Programming'];
 
-        return view('dashboard', compact('section', 'content', 'upcomingSessions', 'subjectTags'));
+
+        return view('dashboard', compact('section', 'content', 'notifications', 'upcomingSessions', 'subjectTags'));
     }
     
     public function profile()
@@ -33,29 +41,42 @@ class UserDashboardController extends Controller
 
         $subjectTags = ['Mathematics', 'English', 'Programming'];
 
+        if (Auth::check()) {
+            $notifications = BookingNotification::where('user_id', Auth::user() -> id)->get();
+        } else {
+            $notifications = collect();
+        }
+
         return view('dashboard', [
             'section' => 'profile',
             'content' => 'dashboard.userProfile',
             'upcomingSessions' => $upcomingSessions,
-            'subjectTags' => $subjectTags
+            'subjectTags' => $subjectTags,
+            'notifications' => $notifications,
         ]);
-
     }
 
     public function requests()
     {
         $upcomingSessions = Booking::where('tutee_id', Auth::id())
-        ->whereDate('date', '>=', now())
-        ->orderBy('date', 'asc')
-        ->get();
-    
+            ->whereDate('date', '>=', now())
+            ->orderBy('date', 'asc')
+            ->get();
+
         $subjectTags = ['Mathematics', 'English', 'Programming'];
-        
+
+        if (Auth::check()) {
+            $notifications = BookingNotification::where('user_id', Auth::user() -> id)->get();
+        } else {
+            $notifications = collect();
+        }
+
         return view('dashboard', [
             'section' => 'requests',
             'content' => 'dashboard.requests',
-            'upcomingSessions' => $upcomingSessions, 
-            'subjectTags' => $subjectTags
+            'upcomingSessions' => $upcomingSessions,
+            'subjectTags' => $subjectTags,
+            'notifications' => $notifications,
         ]);
     }
 
