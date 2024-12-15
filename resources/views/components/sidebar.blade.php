42130ec5785055427c38850a2a3920a8b85f10ea
Whos-Andrei-Go <23104518@usc.edu.ph>
2024-12-15 22:48:03 +0800

    push 2

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-start mt-8 px-4 space-y-2">
         <a href="{{ route('admin.dashboard') }}" class="text-gray-300 hover:text-gray-100 block px-4 py-2 rounded-md">Home</a>
         <a href="{{ route('admin.manageTutorApplications') }}" class="text-gray-300 hover:text-gray-100 block px-4 py-2 rounded-md">Manage Tutor Applications</a>
-        <a href="{{ route('admin.analytics') }}" class="text-gray-300 hover:text-gray-100 block px-4 py-2 rounded-md">Manage Users</a>
+        <a href="{{ route('admin.manageUsers') }}" class="text-gray-300 hover:text-gray-100 block px-4 py-2 rounded-md">Manage Users</a>
         <a href="{{ route('admin.settings') }}" class="text-gray-300 hover:text-gray-100 block px-4 py-2 rounded-md">Settings</a>
     </div>
 </div>
